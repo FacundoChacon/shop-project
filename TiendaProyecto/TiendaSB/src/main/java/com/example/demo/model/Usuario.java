57315a394756914f033b2944c75c64518f33b751
FacundoChacon <chaconfacu2006@gmail.com>
2026-05-31 10:48:28 -0300

    I've added "Usuario" table with its repositort and service

---
+++ TiendaProyecto/TiendaSB/src/main/java/com/example/demo/model/Usuario.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String rol;
+}
