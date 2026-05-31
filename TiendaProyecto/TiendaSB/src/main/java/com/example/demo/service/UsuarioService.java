0bf9c36d18975caeb2b50cf11ae2ce8fab55c6c4
FacundoChacon <chaconfacu2006@gmail.com>
2026-05-31 14:38:54 -0300

    I've finallized the UsuarioService used by the controlled UsuarioController, also I've added SecurityConfig for UsuarioController and UsuarioService

--- TiendaProyecto/TiendaSB/src/main/java/com/example/demo/service/UsuarioService.java
+++ TiendaProyecto/TiendaSB/src/main/java/com/example/demo/service/UsuarioService.java
@@ -1,4 +1,67 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.UsuarioRequestDTO;
+import com.example.demo.dto.UsuarioResponseDTO;
+import com.example.demo.model.Usuario;
+import com.example.demo.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    //  METODOS
+
+    private Usuario convertirAEntidad(
+            UsuarioRequestDTO dto){
+
+        Usuario usuario = new Usuario();
+
+        usuario.setUsername(
+                dto.getUsername()
+        );
+        usuario.setPassword(
+                passwordEncoder.encode(
+                        dto.getPassword()
+                )
+        );
+        usuario.setRol(
+                dto.getRol()
+        );
+
+        return usuario;
+    }
+
+    private UsuarioResponseDTO convertirAResponseDTO(
+            Usuario usuario){
+
+        UsuarioResponseDTO dto =
+                new UsuarioResponseDTO();
+
+        dto.setId(usuario.getId());
+        dto.setUsername(usuario.getUsername());
+        dto.setRol(usuario.getRol());
+
+        return dto;
+    }
+
+    public UsuarioResponseDTO guardarUsuario(
+            UsuarioRequestDTO dto){
+
+        Usuario usuario =
+                convertirAEntidad(dto);
+
+        Usuario guardado =
+                repository.save(usuario);
+
+        return convertirAResponseDTO(
+                guardado
+        );
+    }
 }
